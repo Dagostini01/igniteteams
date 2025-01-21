1d4bcf9923de574cf91ada2b8461f3786a13fa0f
Dagostini01 <pedro01dagostini@gmail.com>
2025-01-20 22:56:18 -0300

    feat: implementação groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,14 @@
-import {
-    Container,
-} from "./styles";
+import { Header } from "@components/Header";
+import { Container } from "./styles";
+import { HightLight } from "@components/HightLight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
     return (
         <Container>
-            
+            <Header />
+            <HightLight title="Turmas" subtitle="jogue com a turma" />
+            <GroupCard title="Nome da turma" />
         </Container>
     )
 }
