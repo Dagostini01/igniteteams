54c5efdb080227dbda35827b05273d1075767b4b
Dagostini01 <pedro01dagostini@gmail.com>
2025-01-24 19:22:57 -0300

    feat: flatlist de groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { HightLight } from "@components/HightLight";
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
 
 export function Groups() {
+
+    const [groups, setGroups] = useState(['Galera da Rocket', 'Amigos do Ignite'])
+
     return (
         <Container>
             <Header />
-            <HightLight title="Turmas" subtitle="jogue com a turma" />
-            <GroupCard title="Nome da turma" />
+
+            <HightLight
+                title="Turmas"
+                subtitle="jogue com a turma"
+            />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
+
         </Container>
     )
 }
